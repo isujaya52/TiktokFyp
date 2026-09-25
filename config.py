d5b4c91af071f307e8d7c58177e6765fc22fd0d6
isujaya52 <isujaya52@sma.belajar.id>
2026-09-25 09:02:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from pyrogram import Client
 
-API_ID = "19965992"
-API_HASH = "37117b5050f0b8572fc7f245580d1311"
-TOKEN = "8954798663:AAGjrZWYV93Wjw8PQ7lu1t5rQns99m-_-3A"
+API_ID = ""
+API_HASH = ""
+TOKEN = ""
 own = [1928677026]
-LOGS_ID = -1001639005361
+LOGS_ID = 
 app = Client("tiktok_video_bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 
 
